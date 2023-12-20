598d4f63e9bd662395b183d43119c495233fafdc
Samiser <sheney10@gmail.com>
2023-12-20 11:58:15 +0000

    tailscale stuff

--- hosts/nix-lab/configuration.nix
+++ hosts/nix-lab/configuration.nix
@@ -2,6 +2,7 @@
   imports = [
     ./hardware-configuration.nix
     ../../common/modules/services/nomad.nix
+    ../../common/modules/services/tailscale.nix
   ];
 
   virtualisation.podman.enable = true;
@@ -16,21 +17,41 @@
 
   systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
 
-  services.tailscale.enable = true;
-
   age.secrets.nomad-samba-credentials = {
     file = ../../secrets/nomad-samba-credentials.age;
     path = "/etc/nixos/smb-secrets";
   };
 
-  environment.systemPackages = [ pkgs.cifs-utils ];
+  environment.systemPackages = with pkgs; [ 
+    cifs-utils
+    neovim
+    tmux
+  ];
   fileSystems."/mnt/hz" = {
     device = "//u361974-sub2.your-storagebox.de/u361974-sub2";
     fsType = "cifs";
     options = let
       # this line prevents hanging on network split
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=1000"];
+  };
+
+  systemd.services.storageRoute = {
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    description = "Route requests to my Hetzner storage box via default routes";
+    path = [pkgs.bash pkgs.iproute];
+    script = '' 
+      if ! (ip rule | grep "46.4.0.0" -q); then
+        ip rule add to 46.4.0.0/16 lookup main pref 5000
+        ip rule add to 46.4.0.0/16 lookup default pref 5010
+      fi
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+      Restart = "no";
+    };
   };
 
   boot.cleanTmpDir = true;
@@ -41,4 +62,6 @@
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDq1QymyXXTarJHQYXQ83Tg4ZAy4EeCrQvZ8nq5Rs/9ZK8Gznkc0eVF0rF/dW5P+a2DJPefYfBsaYS6BFxJwkxSLK1m2hk/rXMsAMviWbN4gtsWtLCqk7tl6sEMuxkUfaFwU5rMevdNNC9dgY2J4216xjTBvtXNkMjjAfnOmVUk2lyq1VxgyKyL4JhJvB/ZUGbmf8q07JPnka4VuxEhYkbWxm9e00LJ61QYxRRIYM6xBNsjBp3n+GUUEeWEPJKCvPZGTzIdTXXWUFAAcTOqxAtb9vtvgnbI63HU21A4KXRm1aS+VPusgofb5qP/CXfLUCfMpPadXAyUFH1ZXAchSxH0tPkgb1AWQdBYrXX/aD2/cyQFpZAGPe5JOWHSfPaRpHvOJvErEimVBdZOkhGV10ustcM0kJcZEH2zBOrMJrsD7wr+HNoxC8TdW7YyoCvu3H624WqDxSyvepwx4XbXx1Ezlwec8QWi0JIjTjP9RHaCYAgEk3FBK54Bsq/iX5EIwC0= sam@khaos"
   ];
+
+  system.stateVersion = "24.05";
 }

2c64e9346c4926acd3d13138d8ff0dfd9675b811
Samiser <sheney10@gmail.com>
2024-04-07 11:51:32 +0100

    tailscale routing fix

--- common/modules/services/tailscale.nix
+++ common/modules/services/tailscale.nix
@@ -1,3 +1,5 @@
 {
   services.tailscale.enable = true;
+  # exit nodes don't work without this (see https://github.com/tailscale/tailscale/issues/4432)
+  networking.firewall.checkReversePath = "loose";
 }

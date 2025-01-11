98a8e5bc10bd7b20ebb8ab833706993c6a621521
Samiser <github@me.samiser.xyz>
2025-01-11 13:40:00 +0000

    remove deploy-rs, nixos-rebuild is good enough

--- common/modules/pkgs.nix
+++ common/modules/pkgs.nix
@@ -1,79 +1,81 @@
-{ config, pkgs, lib, ... }:
-
-let
-  guiEnabled = config.hostConfig.gui.enable;
-in
 {
-  environment.systemPackages = with pkgs; [
-    # cli tools
-    _1password
-    acpi
-    deploy-rs
-    dig
-    fd
-    ffmpeg
-    fzf
-    imagemagick
-    jq
-    pciutils
-    playerctl
-    pv
-    ripgrep
-    starship
-    stow
-    sysstat
-    tree
-    unzip
-    wget
-    wireguard-tools
-    xclip
-    zsh
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  guiEnabled = config.hostConfig.gui.enable;
+in {
+  environment.systemPackages = with pkgs;
+    [
+      # cli tools
+      _1password
+      acpi
+      dig
+      fd
+      ffmpeg
+      fzf
+      imagemagick
+      jq
+      pciutils
+      playerctl
+      pv
+      ripgrep
+      starship
+      stow
+      sysstat
+      tree
+      unzip
+      wget
+      wireguard-tools
+      xclip
+      zsh
 
-    # dev
-    direnv
-    docker-compose
-    entr
-    git
-    hcloud
-    nixfmt-rfc-style
-    python
-    rust-analyzer
-    tcpdump
+      # dev
+      direnv
+      docker-compose
+      entr
+      git
+      hcloud
+      nixfmt-rfc-style
+      python
+      rust-analyzer
+      tcpdump
 
-    # tui
-    gotop
-    htop
-    neofetch
-    nix-tree
-    tmux
-  ] ++ lib.optionals guiEnabled [
-    alacritty
-    arandr
-    chromium
-    dconf
-    discord
-    feh
-    gimp
-    godot_4
-    mesa-demos
-    mpv
-    mupdf
-    obsidian
-    pavucontrol
-    peek
-    prismlauncher
-    scrot
-    spotify
-    virt-manager
-    steam
-    networkmanagerapplet
-  ];
+      # tui
+      gotop
+      htop
+      neofetch
+      nix-tree
+      tmux
+    ]
+    ++ lib.optionals guiEnabled [
+      alacritty
+      arandr
+      chromium
+      dconf
+      discord
+      feh
+      gimp
+      godot_4
+      mesa-demos
+      mpv
+      mupdf
+      obsidian
+      pavucontrol
+      peek
+      prismlauncher
+      scrot
+      spotify
+      virt-manager
+      steam
+      networkmanagerapplet
+    ];
 
   # allow unfree
   nixpkgs.config.allowUnfree = true;
 
-  nixpkgs.config.permittedInsecurePackages =
-    [ "python-2.7.18.7" "python-2.7.18.8" "electron-24.8.6" ];
+  nixpkgs.config.permittedInsecurePackages = ["python-2.7.18.7" "python-2.7.18.8" "electron-24.8.6"];
 
   nix = {
     package = pkgs.nixFlakes;
--- flake.nix
+++ flake.nix
@@ -4,15 +4,12 @@
     home-manager.url = "github:nix-community/home-manager/master";
     nixos-wsl.url = "github:nix-community/nixos-wsl/main";
     nixos-hardware.url = "github:nixos/nixos-hardware/master";
-    deploy-rs.url = "github:serokell/deploy-rs";
     agenix.url = "github:ryantm/agenix";
     my-neovim.url = "github:Samiser/neovim-config";
   };
 
   outputs = {
-    self,
     nixpkgs,
-    deploy-rs,
     my-neovim,
     ...
   } @ attrs: let
@@ -24,27 +21,11 @@
           ./hosts/${hostname}/configuration.nix
         ];
       };
-    mkNode = hostname: {
-      inherit hostname;
-      sshUser = "root";
-      magicRollback = false;
-
-      profiles.system = {
-        user = "root";
-        path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.${hostname};
-      };
-    };
   in {
     nixosConfigurations = {
       imperium = mkSystem "imperium";
       nix-lab = mkSystem "nix-lab";
       lazarus-wsl = mkSystem "lazarus-wsl";
     };
-
-    deploy.nodes = {
-      nix-lab = mkNode "nix-lab";
-    };
-
-    checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
   };
 }

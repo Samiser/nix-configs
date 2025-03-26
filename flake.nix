0c6865cc3f349571bc2bd9544d4ba422312c7e0e
Samiser <github@me.samiser.xyz>
2025-03-26 14:15:56 +0000

    added initial beanbook config, but it needs refactoring

--- flake.nix
+++ flake.nix
@@ -6,26 +6,43 @@
     nixos-hardware.url = "github:nixos/nixos-hardware/master";
     agenix.url = "github:ryantm/agenix";
     my-neovim.url = "github:Samiser/neovim-config";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     nixpkgs,
+    nix-darwin,
+    home-manager,
     my-neovim,
     ...
   } @ attrs: let
-    mkSystem = hostname:
-      nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+    mkSystem = hostname: system: let
+      isDarwin = builtins.match ".*-darwin" system != null;
+      builder =
+        if isDarwin
+        then nix-darwin.lib.darwinSystem
+        else nixpkgs.lib.nixosSystem;
+    in
+      builder {
+        inherit system;
         specialArgs = attrs // {inherit my-neovim;};
-        modules = [
-          ./hosts/${hostname}/configuration.nix
-        ];
+        modules =
+          [
+            ./hosts/${hostname}/configuration.nix
+          ]
+          ++ nixpkgs.lib.optional isDarwin home-manager.darwinModules.home-manager;
       };
   in {
     nixosConfigurations = {
-      imperium = mkSystem "imperium";
-      nix-lab = mkSystem "nix-lab";
-      lazarus-wsl = mkSystem "lazarus-wsl";
+      imperium = mkSystem "imperium" "x86_64-linux";
+      nix-lab = mkSystem "nix-lab" "x86_64-linux";
+      lazarus-wsl = mkSystem "lazarus-wsl" "x86_64-linux";
+    };
+
+    darwinConfigurations = {
+      beanbook = mkSystem "beanbook" "aarch64-darwin";
     };
   };
 }

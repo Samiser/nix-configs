b30613de84f3f80af2935cda8fb8963114ff06f5
Samiser <github@me.samiser.xyz>
2025-05-06 15:28:33 +0500

    add back linux builder

--- darwin-modules/nix/build-machines.nix
+++ darwin-modules/nix/build-machines.nix
@@ -14,5 +14,20 @@
         maxJobs = 4;
       }
     ];
+
+    linux-builder = {
+      enable = true;
+      ephemeral = true;
+      maxJobs = 4;
+      config = {
+        virtualisation = {
+          darwin-builder = {
+            diskSize = 30 * 1024;
+            memorySize = 4 * 1024;
+          };
+          cores = 4;
+        };
+      };
+    };
   };
 }

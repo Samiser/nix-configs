b405e4d95d2ed35dff16e2a956cd6d5d1d5ab6d7
Samiser <github@me.samiser.xyz>
2025-05-19 16:38:34 +0000

    updating secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   sam = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDq1QymyXXTarJHQYXQ83Tg4ZAy4EeCrQvZ8nq5Rs/9ZK8Gznkc0eVF0rF/dW5P+a2DJPefYfBsaYS6BFxJwkxSLK1m2hk/rXMsAMviWbN4gtsWtLCqk7tl6sEMuxkUfaFwU5rMevdNNC9dgY2J4216xjTBvtXNkMjjAfnOmVUk2lyq1VxgyKyL4JhJvB/ZUGbmf8q07JPnka4VuxEhYkbWxm9e00LJ61QYxRRIYM6xBNsjBp3n+GUUEeWEPJKCvPZGTzIdTXXWUFAAcTOqxAtb9vtvgnbI63HU21A4KXRm1aS+VPusgofb5qP/CXfLUCfMpPadXAyUFH1ZXAchSxH0tPkgb1AWQdBYrXX/aD2/cyQFpZAGPe5JOWHSfPaRpHvOJvErEimVBdZOkhGV10ustcM0kJcZEH2zBOrMJrsD7wr+HNoxC8TdW7YyoCvu3H624WqDxSyvepwx4XbXx1Ezlwec8QWi0JIjTjP9RHaCYAgEk3FBK54Bsq/iX5EIwC0=";
-  nix-lab = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG5M3ta0q0C6t80ntESGSWCKkCVuRyjYoQBSpLyK4zqe root@nix-lab";
+  nix-lab = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC3vwyjOAqTmqpquYjTJaOgee+kiUvWxlU5UzKqBzJUy root@nix-lab";
 in {
   "nomad-samba-credentials.age".publicKeys = [sam nix-lab];
 }

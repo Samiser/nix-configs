d8eb975ff42f05c7fe0ae466122c48dfed4f5385
Samiser <github@me.samiser.xyz>
2026-03-05 16:27:41 +0000

    update caddy plugin hash for caddy 2.11.1

--- nixos-modules/modules/services/caddy.nix
+++ nixos-modules/modules/services/caddy.nix
@@ -21,7 +21,7 @@ in {
 
     services.caddy.package = pkgs.caddy.withPlugins {
       plugins = ["github.com/caddy-dns/cloudflare@v0.2.2"];
-      hash = "sha256-ea8PC/+SlPRdEVVF/I3c1CBprlVp1nrumKM5cMwJJ3U=";
+      hash = "sha256-biQdtiscdmrwP6VUsuXmZrvcdewl+g50kdmab5lhE0s=";
     };
   };
 }

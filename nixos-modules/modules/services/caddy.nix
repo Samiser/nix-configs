a0164b9ef0bf86870872299bb458d127dbb28db0
Samiser <github@me.samiser.xyz>
2026-06-05 17:53:21 +0100

    update caddy withPlugins hash after version bump

--- nixos-modules/modules/services/caddy.nix
+++ nixos-modules/modules/services/caddy.nix
@@ -21,7 +21,7 @@ in {
 
     services.caddy.package = pkgs.caddy.withPlugins {
       plugins = ["github.com/caddy-dns/cloudflare@v0.2.2"];
-      hash = "sha256-biQdtiscdmrwP6VUsuXmZrvcdewl+g50kdmab5lhE0s=";
+      hash = "sha256-qEA6058svI8Q6yE97OkfnGWC8ayI3x8y2iU7PGkJ3Do=";
     };
   };
 }

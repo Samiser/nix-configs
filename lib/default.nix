19ddbbc75c47110accbebaacf7fe974091fd2d2e
Samiser <github@me.samiser.xyz>
2026-06-06 15:15:28 +0100

    integrate nix-github-actions for nix builds in cicd

--- lib/default.nix
+++ lib/default.nix
@@ -6,8 +6,14 @@
     inherit (hosts) nixosConfigurations;
   };
   shells = import ./shells.nix {inherit inputs;};
+  ci = import ./ci.nix {
+    inherit inputs;
+    inherit (hosts) nixosConfigurations darwinConfigurations;
+    devShells = shells;
+  };
 in {
   inherit (hosts) nixosConfigurations darwinConfigurations;
   inherit (deploy) deploy;
+  inherit (ci) githubActions checks;
   devShells = shells;
 }

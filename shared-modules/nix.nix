aa56f5a6751cbb9ee8dbf19ede20270985ee1f67
Samiser <github@me.samiser.xyz>
2026-07-22 10:58:34 +0000

    remove garnix (rip)

--- shared-modules/nix.nix
+++ shared-modules/nix.nix
@@ -3,12 +3,10 @@ _: {
     experimental-features = ["nix-command" "flakes"];
     substituters = [
       "https://cache.samiser.xyz/main"
-      "https://cache.garnix.io"
       "https://noctalia.cachix.org"
     ];
     trusted-public-keys = [
       "main:xTlqL+c6HRCxNLtRdVu+TElyY+HD9WiXQn0fSetkbFk="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
     ];
   };

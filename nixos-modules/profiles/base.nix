ae7aae3dab7f26e693b30f88135cace5d0b449bc
Samiser <github@me.samiser.xyz>
2026-07-23 23:42:00 +0000

    ssh config change

--- nixos-modules/profiles/base.nix
+++ nixos-modules/profiles/base.nix
@@ -13,7 +13,13 @@
     ]);
 
   services = {
-    openssh.enable = true;
+    openssh = {
+      enable = true;
+      settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+      };
+    };
     tailscale.enable = true;
   };
 

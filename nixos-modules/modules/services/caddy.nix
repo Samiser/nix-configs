28d7d71bfaedab87bebb6a6e846591c57ae68835
Samiser <github@me.samiser.xyz>
2026-07-25 22:03:20 +0000

    update caddy w plugins

--- nixos-modules/modules/services/caddy.nix
+++ nixos-modules/modules/services/caddy.nix
@@ -3,11 +3,16 @@
   lib,
   pkgs,
   ...
-}: let
+}:
+let
   caddyCfg = config.services.caddy;
-in {
+in
+{
   config = lib.mkIf caddyCfg.enable {
-    networking.firewall.allowedTCPPorts = [80 443];
+    networking.firewall.allowedTCPPorts = [
+      80
+      443
+    ];
 
     age.secrets.caddy-cloudflare-key = {
       file = ../../../secrets/caddy-cloudflare-key.age;
@@ -20,8 +25,8 @@ in {
     };
 
     services.caddy.package = pkgs.caddy.withPlugins {
-      plugins = ["github.com/caddy-dns/cloudflare@v0.2.2"];
-      hash = "sha256-7g8zDx5RhbptXFyEPtexxkHX8hw/gF001bZ7wX4Mjhs=";
+      plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
+      hash = "sha256-mqIa0wI/VfjDblg0NnkzKllWHXZZPLwHP8xEVSwZuPE=";
     };
   };
 }

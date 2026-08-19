0b683425658cc8b5bffdf71e574e9c76b463255c
Samiser <github@me.samiser.xyz>
2026-08-19 16:46:19 +0100

    vendor omniwm module until it gets merged
    tracking https://github.com/NixOS/nixpkgs/pull/553034

--- pkgs/by-name/om/omniwm/package.nix
+++ pkgs/by-name/om/omniwm/package.nix
@@ -1,52 +1,58 @@
 {
-  lib,
   fetchurl,
-  gitUpdater,
   libarchive,
+  lib,
   stdenvNoCC,
+  nix-update-script,
 }:
-
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "omniwm";
-  version = "0.6.0";
+  version = "0.6.2";
 
   src = fetchurl {
     url = "https://github.com/BarutSRB/OmniWM/releases/download/v${finalAttrs.version}/OmniWM-v${finalAttrs.version}.zip";
-    hash = "sha256-XGGSvvtU7m9Ffe7t9ob5LAIzNhEPNZPGBjd9he2NRi4=";
+    hash = "sha256-Iyv5HevupYlGW/EjC5r8ePdu2zg6n6028dB9b57U3rQ=";
   };
 
-  dontUnpack = true;
+  sourceRoot = "OmniWM.app";
 
-  strictDeps = true;
   __structuredAttrs = true;
+  strictDeps = true;
+
+  # Using bsdtar instead of unzip as unzip breaks .app codesigning.
+  # Unpack using bsdtar before installation instead.
+  unpackCmd = # bash
+    ''bsdtar -xf "$curSrc"'';
 
   nativeBuildInputs = [ libarchive ];
 
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/Applications
-    bsdtar -xf $src -C $out/Applications
+    mkdir -p "$out/Applications/OmniWM.app"
+    cp -r . "$out/Applications/OmniWM.app"
 
-    mkdir -p $out/bin
-    ln -s $out/Applications/OmniWM.app/Contents/MacOS/OmniWM $out/bin/OmniWM
-    ln -s $out/Applications/OmniWM.app/Contents/MacOS/omniwmctl $out/bin/omniwmctl
+    # Symlink executables to bin
+    mkdir -p "$out/bin"
+    ln -s "$out/Applications/OmniWM.app/Contents/MacOS/OmniWM" "$out/bin/OmniWM"
+    ln -s "$out/Applications/OmniWM.app/Contents/MacOS/omniwmctl" "$out/bin/omniwmctl"
 
     runHook postInstall
   '';
 
-  passthru.updateScript = gitUpdater {
-    url = "https://github.com/BarutSRB/OmniWM.git";
-    rev-prefix = "v";
-  };
+  passthru.updateScript = nix-update-script { };
+
+  # No version checks are possible as `omniwmctl version` expects a running OmniWM instance with IPC enabled.
 
   meta = {
-    description = "Tiling window manager for macOS inspired by Niri and Hyprland";
+    description = "MacOS Niri and Hyprland inspired tiling window manager";
     homepage = "https://github.com/BarutSRB/OmniWM";
     license = lib.licenses.gpl2Only;
     mainProgram = "OmniWM";
-    maintainers = with lib.maintainers; [ ];
-    platforms = lib.platforms.darwin;
+    platforms = [ "aarch64-darwin" ];
+    maintainers = with lib.maintainers; [
+      samiser
+    ];
     sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
   };
 })

0898d0503cd77ac99b5acdec88f221afcf4ce940
Samiser <github@me.samiser.xyz>
2026-08-29 22:58:57 +0100

    bump way-secure to head

--- pkgs/by-name/wa/way-secure/package.nix
+++ pkgs/by-name/wa/way-secure/package.nix
@@ -7,16 +7,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "way-secure";
-  version = "0.2.0";
+  version = "0.2.0-unstable-2026-08-18";
 
   src = fetchFromSourcehut {
     owner = "~whynothugo";
     repo = "way-secure";
-    rev = "v${finalAttrs.version}";
-    hash = "sha256-ppbCl7NGa8nW6k4zRP3dubOr5LwCuECjbSExDstxQ5o=";
+    rev = "51ff3bb4f30305e698d95d8d0dfcb79982f602b6";
+    hash = "sha256-fyoSNS2R+Pr8xQEUj7rkFKCfrQ95VeH+nwaLCLNNfpw=";
   };
 
-  cargoHash = "sha256-AE+pqQaqQp9C55IL1NOAP7pSZ2Pkg46g1x5oup+IBHw=";
+  cargoHash = "sha256-QzPJPUQjwAtqJiCqQM8lmtdxQDOLTsZrIIpjkyKNDNI=";
 
   nativeBuildInputs = [ installShellFiles ];
 
@@ -27,7 +27,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
   meta = {
     description = "Helper to create Wayland security contexts via security-context-v1";
     homepage = "https://git.sr.ht/~whynothugo/way-secure";
-    changelog = "https://git.sr.ht/~whynothugo/way-secure/tree/v${finalAttrs.version}/item/CHANGELOG.md";
+    changelog = "https://git.sr.ht/~whynothugo/way-secure/tree/${finalAttrs.src.rev}/item/CHANGELOG.md";
     license = lib.licenses.isc;
     maintainers = with lib.maintainers; [ samiser ];
     mainProgram = "way-secure";
